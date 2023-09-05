367d92564ebb2ab2f09a30e0353e47972019c4bb
printer_scanner <abbey@fictivekin.com>
2023-09-05 18:59:13 +0200

    update

--- pages/[slug].js
+++ pages/[slug].js
@@ -3,6 +3,7 @@ import Head from "next/head";
 import { getDatabase, getPage, getBlocks } from "../lib/notion";
 import Link from "next/link";
 import { databaseId } from "./index.js";
+import styles from "./post.module.css";
 
 export const Text = ({ text }) => {
 	if (!text) {
